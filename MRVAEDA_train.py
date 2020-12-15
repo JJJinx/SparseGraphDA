dfe812a52ae17fc0f2793ab6aebb5a1aeb806ee2
Nick Xu <34965861+JJJinx@users.noreply.github.com>
2020-12-15 07:29:07 -0600

    Add files via upload

--- MRVAEDA_train.py
+++ MRVAEDA_train.py
@@ -1,41 +1,36 @@
 # coding=utf-8
-import torch
-print(torch.cuda.is_available())
-
 import os
 import time
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-import random
-import numpy as np
-from torch import nn
-from torch.utils.data import Dataset, DataLoader
-import torch.nn.functional as F
 import itertools
 from argparse import ArgumentParser
+import random
+import numpy as np
+
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+
 from dataset.DomainData import DomainData
 from data_vis import degree_level_acc
 from model.MRVGAE_model import *
 from data_process import *
-#print all value
-torch.set_printoptions(profile="full")
+# #print all value
+# torch.set_printoptions(profile="full")
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 parser = ArgumentParser()
 parser.add_argument("--source", type=str, default='acm')
 parser.add_argument("--target", type=str, default='dblp')
 parser.add_argument("--seed", type=int,default=200)
-parser.add_argument("--encoder_dim", type=int, default=16)
+parser.add_argument("--lr", type=float,default=0.001)
 
 args = parser.parse_args()
 seed = args.seed
-encoder_dim = args.encoder_dim
 
-id = "source: {}, target: {}, seed: {}, encoder_dim: {}"\
-    .format(args.source, args.target, seed,  encoder_dim)
-
+id = "source: {}, target: {}, seed: {}"\
+    .format(args.source, args.target, seed)
 print(id)
 
-rate = 0.0
 #要固定输出后两个需要；即np控制mask，而torch控制模型初始参数
 # random.seed(seed)
 # np.random.seed(seed)
@@ -56,14 +51,15 @@ target_data = dataset[0]
 
 label_min_index = source_data.y.min()
 label_max_index = source_data.y.max()
+node_label_num = label_max_index-label_min_index+1
 # mapping from node type to node pair type
-ntype_etype_mapping = torch.zeros([label_max_index-label_min_index+1,label_max_index-label_min_index+1],dtype=torch.long)
-i = 0
-for src_node_type in range(label_min_index,label_max_index+1):
-    for tgt_node_type in range(src_node_type,label_max_index+1):
-        ntype_etype_mapping[src_node_type,tgt_node_type] = i
-        ntype_etype_mapping[tgt_node_type,src_node_type] = i
-        i+=1
+# ntype_etype_mapping = torch.zeros([label_max_index-label_min_index+1,label_max_index-label_min_index+1],dtype=torch.long)
+# i = 0
+# for src_node_type in range(label_min_index,label_max_index+1):
+#     for tgt_node_type in range(src_node_type,label_max_index+1):
+#         ntype_etype_mapping[src_node_type,tgt_node_type] = i
+#         ntype_etype_mapping[tgt_node_type,src_node_type] = i
+#         i+=1
 ## data processing
 '''
 require
@@ -85,25 +81,24 @@ self_loop = torch.arange(target_data.x.shape[0])
 self_loop = self_loop.unsqueeze(1).repeat(1,2)
 edge_index = target_data.edge_index
 tgt_edge_index_sl = torch.cat([edge_index.T,self_loop]).T #[2,N]
-
-source_node_num = source_data.x.shape[0]
-target_node_num = target_data.x.shape[0]
-print(source_data.edge_index.shape)
-print(target_data.edge_index.shape)
-t = time.time()
-src_all_node_pair,src_all_node_pair_label = generate_all_node_pair(source_node_num,src_edge_index_sl,source_data.y,ntype_etype_mapping)
-
-raise RuntimeError
-tgt_all_node_pair,tgt_all_node_pair_label = generate_all_node_pair(target_node_num,tgt_edge_index_sl,target_data.y,ntype_etype_mapping)
-print(src_all_node_pair_label)
-print(time.time()-t)
-np.savetxt('acm_all_node_pair_label.csv',src_all_node_pair_label.numpy(),delimiter=',')
-np.savetxt('dblp_all_node_pair_label.csv',tgt_all_node_pair_label.numpy(),delimiter=',')
 ## generate train graph
-source_graph = dgl.graph(src_edge_index_sl[0],src_edge_index_sl[1])
-target_graph = dgl.graph(tgt_edge_index_sl[0],tgt_edge_index_sl[1])
+source_graph = dgl.graph((src_edge_index_sl[0],src_edge_index_sl[1]))
+target_graph = dgl.graph((tgt_edge_index_sl[0],tgt_edge_index_sl[1]))
 source_graph.ndata['feats'] = source_data.x
 target_graph.ndata['feats'] = target_data.x
+##generate all node pair label
+source_node_num = source_data.x.shape[0]
+target_node_num = target_data.x.shape[0]
+src_all_node_pair,src_all_node_pair_label,max_np_label =generate_all_node_pair(source_node_num,src_edge_index_sl,source_data.y,
+                                                                                node_label_num,source_graph.adjacency_matrix()) # tensor,tensor
+tgt_all_node_pair,tgt_all_node_pair_label,max_np_label = generate_all_node_pair(target_node_num,tgt_edge_index_sl,target_data.y,
+                                                                                node_label_num,target_graph.adjacency_matrix())
+min_np_label = 0 # no_edge_existence
+max_np_label = max_np_label.item() 
+#np.savetxt('acm_all_node_pair_label.csv',src_all_node_pair_label.numpy(),delimiter=',')
+#np.savetxt('dblp_all_node_pair_label.csv',tgt_all_node_pair_label.numpy(),delimiter=',')
+#raise RuntimeError
+
 ## dataloader
 class Node_Pair_Dataset(Dataset):
     def __init__(self,node_pairs,node_pair_labels):
@@ -120,41 +115,49 @@ src_dataloader = DataLoader(source_np_dataset, batch_size=2048,
                         shuffle=True, num_workers=4)
 tgt_dataloader = DataLoader(target_np_dataset, batch_size=2048,
                         shuffle=True, num_workers=4)
-## model
-# TODO assert this two items are the same
-input_dim = source_data.x.shape[1]
-in_dim = dataset.num_features
-hidden_dim = [] #TODO
-# THE model TODO
+
+## model TODO 完成整个tl模型的设计
+#input_dim = source_data.x.shape[1]
+categorical_dim = max_np_label-min_np_label+1
+input_dim = dataset.num_features
+hidden_dim = [1024     ,1024     ,32*categorical_dim ,32                             ,64               ,256             ]
+#              0         1            2                3                               4                 5
+#hidden_dim= [gcn1_out ,gcn2_out ,vi_mlp_out         ,decoder_mlp1_in(embedding dim) ,decoder_mlp1_out ,decoder_mlp2_out]
 class VGAE(torch.nn.Module):
     def __init__(self, in_dim,hidden_dim, **kwargs):
         super(VGAE, self).__init__()
-        self.private_encoder = private_encoder(in_dim,hidden_dim[0])
+        self.private_encoder_source = private_encoder(in_dim,hidden_dim[0])
+        self.private_encoder_target = private_encoder(in_dim,hidden_dim[0])
         self.shared_encoder = shared_encoder(hidden_dim[0],hidden_dim[1])
-        self.VI = VI(hidden_dim[1],hidden_dim[2])
-        self.shared_decoder = shared_decoder(hidden_dim[2],hidden_dim[3])
-        self.private_decoder = private_decoder(hidden_dim[3],hidden_dim[4])
-
-    def forward(self, x, edge_index):
-        x = self.private_encoder(x,edge_index) 
+        #self.VI = VI(hidden_dim[1],hidden_dim[2])
+        self.shared_decoder = shared_decoder(hidden_dim[3],hidden_dim[4])
+        self.private_decoder_source = private_decoder(hidden_dim[4],hidden_dim[5])
+        self.private_decoder_target = private_decoder(hidden_dim[4],hidden_dim[5])
+        self.A_classifier = relation_classifier()
+        self.discriminator = None
+    def forward(self, x, edge_index,domain):
+        if domain == 'source'
+        x = self.private_encoder_source(x,edge_index) 
         h = self.shared_encoder(x,edge_index)
         # h = torch.mm(h,h.T)*hij 
-        z = self.VI(hij)
+        #z = self.VI(hij)
         h = self.shared_decoder(z)
-        A = self.private_decoder(h)
-        return A
-
+        X = self.private_decoder_source(h)
+        A = self.A_classifier(h)
+        else:
+            pass
+        return A,X
+    def inference(self):
+        pass
 models = VGAE(in_dim,hidden_dim)
 optimizer = torch.optim.Adam(models.parameters(), lr=3e-3)
-##  TODO  sheduler
-
-## training
+scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,np.arange(1,200,50), gamma=0.1, last_epoch=-1)
+## training TODO
 def train():
     models.train()
     optimizer.zero_grad()
-    A_pred = models(source_data.x, source_data.edge_index)
+    A_pred,X_recon = models(source_data.x, source_data.edge_index,domain)
     # enc
-
     src_idx = node_pairs[0]
     dst_idx = node_pairs[1]
     h_src = H[src_idx]  # [num_src,dh]
@@ -168,18 +171,12 @@ def train():
     #dec
 
     #
-
-
     loss = loss_func(A_pred[source_data.train_mask], source_data.y[source_data.train_mask])
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-
+# TODO
 def test():
     pass
-
-def predict():
-    pass
-
 
 print('------------End of training----------')
--- data_process.py
+++ data_process.py
@@ -1,4 +1,7 @@
+import dgl
 import torch
+import numpy as np
+
 def edge_index_to_adj(node_num,edge_index):
     '''
         edge_index :: [n,2]
@@ -13,21 +16,38 @@ def Is_self_loop_exist(edge_index,node_num):
     symble = (num_self_loop==node_num)
     return symble
 
-def generate_all_node_pair(node_num,edge_index,node_label,ntype_etype_mapping):
+def generate_all_node_pair(node_num,edge_index,node_label,node_label_num,adj):
     '''
+    input
+        edge_index :: tensor shape [2,N] 
+        node_label_num :: tensor shape [1]
+        adj :: sparse format for adjacent matrix
     return
-        all_node_pair :: shape [N ,2]
-        all_node_pair_label :: shape [N]
+        all_node_pair :: tensor shape [N ,2]
+        all_node_pair_label :: tensor shape [N]. For all node pairs,no matter whether the edge exists
     '''
-    #
+    #row = np.expand_dims(np.expand_dims(np.arange(node_num),axis=1).repeat(node_num,axis=1),axis=2)
+    #col = np.expand_dims(np.expand_dims(np.arange(node_num),axis=0).repeat(node_num,axis=0),axis=2)
+    #all_node_pair = np.concatenate((row,col),axis=2)
     edge_index = edge_index.T
     row = torch.arange(node_num)
     col = torch.arange(node_num)
     row = row.unsqueeze(1).repeat(1,node_num).unsqueeze(2)
     col = col.unsqueeze(0).repeat(node_num,1).unsqueeze(2)
-    all_node_pair = torch.cat((row,col),dim=2).view(-1,2)
-    all_node_pair_label = torch.zeros(all_node_pair.shape[0],dtype=torch.long)
-    # traverse the edge index is faster
+    all_node_pair = torch.cat((row,col),dim=2)
+
+    #ntype_label_for_each_pair = node_label.numpy()[all_node_pair]
+    ntype_label_for_each_pair = node_label[all_node_pair]
+    #make [n_i,n_j]和[n_j,n_i] has the same np label
+    src = torch.min(ntype_label_for_each_pair,dim=-1).values # shape [N,N]
+    dst = torch.max(ntype_label_for_each_pair,dim=-1).values # shape [N,N]
+    # src = ntype_label_for_each_pair.min(axis=-1)
+    # dst = ntype_label_for_each_pair.max(axis=-1)
+    #label the existence edges; make not existence edges' label =0
+    all_node_pair_label = ((node_label_num+node_label_num-(src-1))*((src-1)+1)/2+(dst-src)+1).type(torch.int32)
+    max_np_label = all_node_pair_label.max()
+    all_node_pair_label = all_node_pair_label*adj.to_dense().type(torch.int32)
+    #all_node_pair_label = ((int(node_label_num)+int(node_label_num)-(src-1))*((src-1)+1)/2+(dst-src)+1)*adj.to_dense().numpy()
     '''
     for node_pair in edge_index:
         src = node_pair[0]  #source node index
@@ -38,35 +58,20 @@ def generate_all_node_pair(node_num,edge_index,node_label,ntype_etype_mapping):
         i = torch.nonzero(torch.all(torch.eq(all_node_pair,node_pair),dim=-1))
         all_node_pair_label[i] = node_pair_label # value this node_pair's type
     '''
-    node_label_num = torch.tensor([node_label.max()-node_label.min()+1]*all_node_pair.shape[0],dtype=torch.long)
-    #首先获得节点对中每对节点,得到其节点标签的pair TODO
-    ntype_label_for_each_pair = node_label[all_node_pair]
-    #利用节点标签的pair获取src和dst，目的是使得[n_i,n_j]和[n_j,n_i]拥有相同的标签 TODO
-    src = torch.min(ntype_label_for_each_pair,dim=-1).values
-    dst = torch.max(ntype_label_for_each_pair,dim=-1).values
-    #利用关系式求出node pair的标签all_node_pair_label  TODO
-    all_node_pair_label = ((node_label_num+node_label_num-(src-1))*((src-1)+1)/2+(dst-src)+1).long()
-    print(all_node_pair_label.view(5,5))
-    raise RuntimeError
-    return all_node_pair,all_node_pair_label
+    return all_node_pair,all_node_pair_label,max_np_label
 
 if __name__ == "__main__":
     node_label = torch.tensor([0,1,2,0,2])
     edge_index = torch.tensor([[0,0,0,1,1,2,2,3,4],[0,2,4,1,4,2,3,3,4]])
-    ntype_etype_mapping = torch.zeros([3,3],dtype=torch.long)
-    i = 1
-    for src_node_type in range(0,3):
-        for tgt_node_type in range(src_node_type,3):
-            ntype_etype_mapping[src_node_type,tgt_node_type] = i
-            ntype_etype_mapping[tgt_node_type,src_node_type] = i
-            i+=1
-    node_pair,node_pair_label = generate_all_node_pair(5,edge_index,node_label,ntype_etype_mapping)
-    print(node_pair)
-    print(node_pair_label.view(5,5))
-    ##检查一对元素是否在edge index中
-    # check = torch.tensor([5,3])
-    # print(edge_index.T)
-    # print(torch.eq(edge_index.T,check))
-    # print(torch.all(torch.eq(edge_index.T,check),dim=-1))
-    # print(torch.all(torch.eq(edge_index.T,check),dim=-1).any())
+    g = dgl.graph((edge_index[0],edge_index[1]))
+    # ntype_etype_mapping = torch.zeros([3,3],dtype=torch.long)
+    # i = 1
+    # for src_node_type in range(0,3):
+    #     for tgt_node_type in range(src_node_type,3):
+    #         ntype_etype_mapping[src_node_type,tgt_node_type] = i
+    #         ntype_etype_mapping[tgt_node_type,src_node_type] = i
+    #         i+=1
+    node_pair,node_pair_label,max_np_label = generate_all_node_pair(5,edge_index,node_label,3,g.adjacency_matrix())
+    #print(node_pair)
+    print(node_pair_label)
     
